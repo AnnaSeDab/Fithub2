b70115de471420e24fb6d034d8a8ada760a01af6
AnnaSeDab <>
2023-09-17 15:28:40 +0000

    add: merchandise urls views and template

--- merchandise/views.py
+++ merchandise/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Product, Category
 
 # Create your views here.
+
+
+def merchandise(request):
+    """A view to return the page with all the merchandise, sorting and search"""
+
+    products = Product.objects.all()
+    
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'merchandise/merchandise.html', context)
+
