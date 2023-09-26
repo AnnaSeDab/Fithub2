e126a9d0f074304843d81cbc54a7e16294ea719c
AnnaSeDab <>
2023-09-26 19:42:44 +0000

    add: plans details pae, views and urls

--- plans/urls.py
+++ plans/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.plans, name='plans'),
+    path('<plan_id>', views.plan_details, name='plan_details'),
 ]
--- plans/views.py
+++ plans/views.py
@@ -16,3 +16,15 @@ def plans(request):
     }
 
     return render(request, 'plans/plans.html', context)
+
+
+def plan_details(request, plan_id):
+    """A view to return the page with chosen plan details"""
+
+    plan = get_object_or_404(FitnessPlan, pk=plan_id)
+
+    context = {
+        'plan': plan,
+    }
+
+    return render(request, 'plans/plan_details.html', context)
