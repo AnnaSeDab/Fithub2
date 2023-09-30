882e66a0ff3cc3d080b9458d48ccff3188fbff26
AnnaSeDab <>
2023-09-30 10:45:01 +0000

    add: order history and user details form in pforile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
+
+from checkout.models import Order
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
 
     template = 'profiles/profile.html'
     context = {
@@ -17,22 +28,46 @@ def profile(request):
 
 
 def my_info(request):
-    """ Display the user's profile with user profile """
+    """ Display the user's profile with user info """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
 
     template = 'profiles/my_info.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
 
 
 def my_orders(request):
-    """ Display the user's profile with user profile """
+    """ Display the user's profile with user order history"""
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    orders = profile.orders.all()
+
     template = 'profiles/my_orders.html'
+    context = {
+        'profile': profile,
+        'orders': orders,
+    }
+
+    return render(request, template, context)
+
+
+def my_plan(request):
+    """ Display the user's profile with user fitness plan """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    template = 'profiles/my_plan.html'
     context = {
         'profile': profile,
     }
@@ -40,13 +75,18 @@ def my_orders(request):
     return render(request, template, context)
 
 
-def my_plan(request):
-    """ Display the user's profile with user profile """
-    profile = get_object_or_404(UserProfile, user=request.user)
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
 
-    template = 'profiles/my_plan.html'
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
     context = {
-        'profile': profile,
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
