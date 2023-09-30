1825d6952722052dfdc6eeacf4fb9cd2f988c544
AnnaSeDab <>
2023-09-30 12:10:26 +0000

    add: orders history url path and page

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('my_info', views.my_info, name='my_info'),
     path('my_plan', views.my_plan, name='my_plan'),
     path('my_orders', views.my_orders, name='my_orders'),
+    path('order_history/<order_number>', views.order_history, name='order_history'),
 ]
