9aaac4b35bd00c34bf372d2c112b9819a75111ee
AnnaSeDab <>
2023-09-30 09:39:02 +0000

    add: views urls and pages for the profile page

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('my_info', views.my_info, name='my_info'),
+    path('my_plan', views.my_plan, name='my_plan'),
+    path('my_orders', views.my_orders, name='my_orders'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,52 @@
+from django.shortcuts import render, get_object_or_404
 
-from django.shortcuts import render
+
+from .models import UserProfile
 
 
 def profile(request):
     """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, template, context)
+
+
+def my_info(request):
+    """ Display the user's profile with user profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    template = 'profiles/my_info.html'
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, template, context)
+
+
+def my_orders(request):
+    """ Display the user's profile with user profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    template = 'profiles/my_orders.html'
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, template, context)
+
+
+def my_plan(request):
+    """ Display the user's profile with user profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    template = 'profiles/my_plan.html'
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
