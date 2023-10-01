87356b65f29a4e776031e7dfe54ca4fb4ab94703
AnnaSeDab <>
2023-10-01 19:26:57 +0000

    add: fitness day edit form

--- plans/forms.py
+++ plans/forms.py
@@ -15,3 +15,17 @@ class PlanForm(forms.ModelForm):
 
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
+
+class DayForm(forms.ModelForm):
+
+    class Meta:
+        model = DayPlan
+        fields = ('name', 'friendly_name', 'description',
+            'video_url', 'is_rest')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
--- plans/urls.py
+++ plans/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('<plan_id>', views.plan_details, name='plan_details'),
     path('<plan_id>/<day_id>', views.day, name='day'),
     path('complete/<plan_id>/<day_id>', views.complete, name='complete'),
-    path('add/', views.add_plan, name='add_plan'),
+    path('add_plan/', views.add_plan, name='add_plan'),
+    path('add_day/', views.add_day, name='add_day'),
 ]
