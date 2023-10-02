2230acf1adab5192463c3a50874064122b57363b
AnnaSeDab <>
2023-10-02 12:58:56 +0000

    fix: plan complete button for users that do not own this plan

--- plans/views.py
+++ plans/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
+from profiles.models import UserProfile
 from .models import DayPlan, FitnessPlan, PlanCategory
 from merchandise.models import Product, Category
-
+from profiles.forms import PlanForm, DayForm, FitnessCategoryForm
 
 
 def plans(request):
@@ -52,13 +53,21 @@ def complete(request, plan_id, day_id):
 
     plan = get_object_or_404(FitnessPlan, pk=plan_id)
     day = get_object_or_404(DayPlan, pk=day_id)
+    user = get_object_or_404(UserProfile, user=request.user)
+    
+    if user.fitness_plan == plan:
+        day.completed = True
+        day.save(update_fields=['completed'])
 
-    print(day)
-    day.completed = True
-    day.save(update_fields=['completed'])
+        context = {
+            'day': day,
+        }
 
-    context = {
-        'day': day,
-    }
+        return render(request, 'plans/complete.html', context)
+    
+    else:
+        context = {
+            'plan': plan,
+        }
 
-    return render(request, 'plans/complete.html', context)
+        return render(request, 'plans/plan_details.html', context)
