ce735393f27477bc358a99b82624d3a67bf61633
AnnaSeDab <>
2023-10-02 18:20:18 +0000

    add: prepare for deployment

--- fithub/settings.py
+++ fithub/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-)hu!!!$@jqha31(m8#-n7(nz&6b)21pki3fz!y#*)25!ws(%#h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-annasedab-fithub2-wojmqjnwrn.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['fithubv2-fe0911af64c8.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +121,18 @@ WSGI_APPLICATION = 'fithub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
@@ -183,3 +190,5 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'fithub@example.com'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
