04c37e1ec8130d835881cda4104eb3be2e61403a
AnnaSeDab <>
2023-10-02 13:00:43 +0000

    add: edit and delete plan functionality

--- plans/urls.py
+++ plans/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<plan_id>', views.plan_details, name='plan_details'),
     path('<plan_id>/<day_id>', views.day, name='day'),
     path('complete/<plan_id>/<day_id>', views.complete, name='complete'),
+    path('edit/<int:plan_id>/', views.edit_plan, name='edit_plan'),
+    path('delete/<int:plan_id>/', views.delete_plan, name='delete_plan'),
 ]
--- plans/views.py
+++ plans/views.py
@@ -71,3 +71,46 @@ def complete(request, plan_id, day_id):
         }
 
         return render(request, 'plans/plan_details.html', context)
+
+
+@login_required
+def edit_plan(request, plan_id):
+    """ Edit a fitness plan """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    plan = get_object_or_404(FitnessPlan, pk=plan_id)
+    if request.method == 'POST':
+        form = PlanForm(request.POST, request.FILES, instance=plan)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'You have succesfuly updated { plan.name }!')
+            return redirect(reverse('plan_details', args=[plan.id]))
+        else:
+            messages.error(
+                request, 'Your atempt to edit a fitness plan failed. Please make sure the form is valid.')
+    else:
+        form = PlanForm(instance=plan)
+        messages.info(request, f'You are editing {plan.name}')
+
+    template = 'plans/edit_plan.html'
+    context = {
+        'form': form,
+        'plan': plan,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_plan(request, plan_id):
+    """ Delete a fitness plan from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    plan = get_object_or_404(FitnessPlan, pk=plan_id)
+    plan.delete()
+    messages.success(request, f'Fitness plan {plan.name} has been deleted!')
+    return redirect(reverse('plans'))
